9e6d2b487625588b082d95dc2133455a76f09b00
unknown <bhavya.shashidhar30@gmail.com>
2019-10-16 21:39:08 +1300

    python programming

---
+++ python_code.py
@@ -0,0 +1,7 @@
+iii
+print("this notebook will be mainly used for the capstone project.")
+import pandas as pd
+import Numpy as np
+print("Hello Capestone Project Course")
+"This notebook will be mainly used for the capstone project."
+
